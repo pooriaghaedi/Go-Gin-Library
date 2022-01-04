fe859bdb4938d7a18cd76eacf8b11cb9ae4f97db
pooriaghaedi <pooria.ghaedi@gmail.com>
2022-01-04 19:29:25 +0330

    struct, get books and get books by ID added

--- README.md
+++ README.md
@@ -2,9 +2,13 @@
 Learning Gin with writing a Library backend
 
 ### To Do
-- [ ] books must have name,IBSN,author,publisher,category,photo
-- [ ] create index path to get list of books
+- [x] books must have name,IBSN,author,publisher,category,photo
+- [x] create index path to get list of books
+- [x] create index path to get books bi ID
 - [ ] create path to add new books to library
+- [ ] create path to delete existing book from library
 - [ ] a path to edit existing books
 - [ ] add authentication for editing and adding the books
 - [ ] updating photos for books
+- [ ] add database for storing the data
+
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type user struct {
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	IBSN      string `json:"ibsn"`
+	Author    string `json:"author"`
+	Publisher string `json:"publisher"`
+	Category  string `json:"category"`
+	Photo     string `json:"photo"`
+}
+
+var books = []user{
+	{ID: "1", Name: "Networking and Kubernetes", IBSN: "978-1492081654", Author: "James Strong", Publisher: "O'Rielly", Category: "DevOps"},
+}
+
+// postusers adds an user from JSON received in the request body.
+func postBooks(c *gin.Context) {
+	var newBook user
+
+	if err := c.BindJSON(&newBook); err != nil {
+		return
+	}
+
+	// Add the new book to the slice.
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
+func getBookByID(c *gin.Context) {
+	id := c.Param("id")
+
+	// Loop over the list of users, looking for
+	// an user whose ID value matches the parameter.
+	for _, a := range books {
+		if a.ID == id {
+
+			c.IndentedJSON(http.StatusOK, a)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "User not found"})
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/v1/books", getBooks)
+	router.POST("/v1/books", postBooks)
+	router.GET("/v1/getBookByID/:id", getBookByID)
+
+	router.Run("0.0.0.0:8080")
+}
+
+// getUsers responds with the list of all Users as JSON.
+func getBooks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, books)
+}
