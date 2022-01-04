bc6a13807c9db336589dc3de776c00aa2261134f
pooriaghaedi <pooria.ghaedi@gmail.com>
2022-01-04 21:35:40 +0330

    database added

--- README.md
+++ README.md
@@ -5,10 +5,11 @@ Learning Gin with writing a Library backend
 - [x] books must have name,IBSN,author,publisher,category,photo
 - [x] create index path to get list of books
 - [x] create index path to get books by ID
-- [ ] create path to add new books to library
+- [x] create path to add new books to library
 - [ ] create path to delete existing book from library
 - [ ] a path to edit existing books
 - [ ] add authentication for editing and adding the books
 - [ ] updating photos for books
 - [ ] add database for storing the data
-
+- [ ] add validation to struct
+- [ ] error handling
--- main.go
+++ main.go
@@ -1,28 +1,39 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 )
 
-type user struct {
-	ID        string `json:"id"`
-	Name      string `json:"name"`
-	IBSN      string `json:"ibsn"`
-	Author    string `json:"author"`
-	Publisher string `json:"publisher"`
-	Category  string `json:"category"`
-	Photo     string `json:"photo"`
+var db *gorm.DB
+var err error
+
+type book struct {
+	ID        string    `json:"id"`
+	Name      string    `json:"name"`
+	IBSN      string    `json:"ibsn"`
+	Author    string    `json:"author"`
+	Publisher string    `json:"publisher"`
+	Category  string    `json:"category"`
+	Photo     string    `json:"photo"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	DeletedAt time.Time `json:"deleted_at"`
 }
 
-var books = []user{
+var books = []book{
 	{ID: "1", Name: "Networking and Kubernetes", IBSN: "978-1492081654", Author: "James Strong", Publisher: "O'Rielly", Category: "DevOps"},
 }
 
+var deletedBooks = []book{}
+
 // postusers adds an user from JSON received in the request body.
 func postBooks(c *gin.Context) {
-	var newBook user
+	var newBook book
 
 	if err := c.BindJSON(&newBook); err != nil {
 		return
@@ -33,6 +44,28 @@ func postBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusCreated, newBook)
 }
 
+func deleteBook(c *gin.Context) {
+	id := c.Param("id")
+	var deleteBook book
+	currentTime := time.Now()
+
+	// if err := c.BindJSON(&deleteBook); err != nil {
+	// 	fmt.Printf("error")
+	// 	return
+	// }
+
+	for _, a := range books {
+		if a.ID == id {
+			a.DeletedAt = currentTime
+			fmt.Println(currentTime.Format("2017-09-07 17:06:06"))
+			c.IndentedJSON(http.StatusAccepted, deleteBook)
+			return
+		}
+	}
+	// Add the new book to the slice.
+
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+}
 func getBookByID(c *gin.Context) {
 	id := c.Param("id")
 
@@ -40,24 +73,32 @@ func getBookByID(c *gin.Context) {
 	// an user whose ID value matches the parameter.
 	for _, a := range books {
 		if a.ID == id {
-
 			c.IndentedJSON(http.StatusOK, a)
 			return
 		}
 	}
-	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "User not found"})
-}
-
-func main() {
-	router := gin.Default()
-	router.GET("/v1/books", getBooks)
-	router.POST("/v1/books", postBooks)
-	router.GET("/v1/getBookByID/:id", getBookByID)
-
-	router.Run("0.0.0.0:8080")
+	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
 }
 
 // getUsers responds with the list of all Users as JSON.
 func getBooks(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, books)
+}
+
+func main() {
+	db, _ = gorm.Open("mysql", "root:'PaS$Wd'@tcp(127.0.0.1:3306)/library?charset=utf8&parseTime=True&loc=Local")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+	db.AutoMigrate(&book{})
+	router := gin.Default()
+	router.GET("/v1/books", getBooks)
+	router.POST("/v1/books", postBooks)
+	router.GET("/v1/books/:id", getBookByID)
+	router.DELETE("/v1/books/:id", deleteBook)
+
+	router.Run("0.0.0.0:8080")
 }
