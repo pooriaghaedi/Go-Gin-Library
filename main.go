7da4e8d53abf6c23b693be24267a174e1ec7f2e1
pooria <p.ghaedi@ronad.ir>
2022-01-09 14:44:04 +0330

    upload book photo

--- main.go
+++ main.go
@@ -113,7 +113,9 @@ func UploadBookcover(c *gin.Context) {
 			c.String(http.StatusBadRequest, "upload file err: %s", err.Error())
 			return
 		} else {
-			db.Model(&Book).Where("id = ?", id).Update(Book.Photo, "23123")
+			if err := db.Model(&Book).Where("id = ?", id).Update(Book.Photo, "23123").Error; err != nil {
+				fmt.Println(err)
+			}
 			c.String(http.StatusOK, "File %s uploaded successfully with fields name=%s and id=%s.", file.Filename, id)
 		}
 
